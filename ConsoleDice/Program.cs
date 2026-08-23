3298ec80bc4b8c23b837e75c7e2fa791af9b03a3
Anthony Savchenko <anthony.savchenko@gmail.com>
2026-08-23 12:00:20 +0300

    Get Started Console

---
+++ ConsoleDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+const string DiceActivitySourceName = "ConsoleDice.Dice";
+
+using var tracerProvider = Sdk
+  .CreateTracerProviderBuilder()
+  .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("getting-started"))
+  .AddSource(DiceActivitySourceName)
+  .AddConsoleExporter()
+  .Build();
+
+var DiceActivitySource = new ActivitySource(DiceActivitySourceName);
+
+using var activity = DiceActivitySource.StartActivity("SayHello");
+
+activity?.SetTag("foo", 1);
+activity?.SetTag("bar", "Hello, World!");
+activity?.SetTag("baz", new int[] { 1, 2, 3 });
+
+activity?.SetStatus(ActivityStatusCode.Ok);
